bf361fdb1bd0eb0dce43500f258a00d5176acafc
AvineetKaur <avineetkaur15@gmail.com>
2025-03-19 19:57:06 +0530

    Added Quote component

---
+++ frontend/src/components/Quote.tsx
@@ -0,0 +1,33 @@
+export const Quote = () => {
+    return (<>
+        <div className="bg-slate-200 h-screen flex justify-center flex-col ">
+            <div className="flex  justify-center">
+                <div className="max-w-lg">
+                    <div className=" bg-amber-300">
+                        The customer support I received was exceptional. The support team went above and beyond to address my concerns.
+                    </div>
+
+                    <div className="bg-red-400">
+                        helllo
+                    </div>
+                    <div className="bg-blue-400">
+                        hjk;l;
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+        {/* <div className="bg-slate-200 h-screen flex justify-center flex-col" >
+            <div className="flex justify-center">
+                <div className="max-w-md  text-2xl font-bold">
+                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns."
+                </div>
+            </div>
+        </div> */}
+
+    </>)
+}
